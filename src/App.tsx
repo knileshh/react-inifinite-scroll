43150427fcfae03a81cb784c516001bbf1f7a4c5
knilesh9 <neutron_stays@protonmail.com>
2024-05-30 13:46:17 +0530

    Need to add styles but because of intersection observer what will happen when the elements open zoomed out?

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useInfiniteQuery } from "@tanstack/react-query";
+import {useInView } from 'react-intersection-observer'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { fetchItems } from "./api/items";
+import { useEffect } from "react";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  const { data, error, status, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["items"],
+    queryFn: fetchItems,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+
+  const {ref, inView} = useInView()
+
+  useEffect(() => {
+      if(inView) {
+        fetchNextPage()
+      }
+  }, [fetchNextPage, inView])
+
+  return status === "pending" ? (
+    <div> Loading...</div>
+  ) : status === "error" ? (
+    <div>{error.message}</div>
+  ) : (
+    <div className="flex flex-col gap-2">
+      {data.pages.map((page) => {
+        return (
+          <div key={page.currentPage} className="flex flex-col gap-2">
+            {page.data.map((item) => {
+              return (
+                <div key={item.id} className="p-4 bg-grayscale-700 rounded-md">
+                  {item.name}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+
+
+      <div ref={ref}> {isFetchingNextPage && 'Loading...'}</div>
+    </div>
+  );
 }
-
-export default App
